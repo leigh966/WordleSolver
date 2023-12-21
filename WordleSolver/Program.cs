2a62904aaa72be05f3f4d6503dc3d98a7d011eb9
leigh966 <leigh966@hotmail.com>
2023-12-21 16:26:34 +0000

    makes guesses purely by ruling words out that contain letters that we know aren't in the answer

--- WordleSolver/Program.cs
+++ WordleSolver/Program.cs
@@ -1,6 +1,48 @@
 ﻿using LeggySetLib;
+using System;
 
 string[] allWords = File.ReadAllLines("words.txt");
 
 ISet<int> badCharacters = new BinarySet64(0,25);
 
+
+bool couldBeThisWord(string word)
+{
+    foreach (char letter in word)
+    {
+        if (badCharacters.Contains(getNumber(letter)))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+string? getGuess()
+{
+    foreach(string word in allWords)
+    {
+        if(couldBeThisWord(word))
+        {
+            return word;
+        }
+    }
+    return null;
+}
+
+int getNumber(char letter)
+{
+    return (int)(letter) -  97;
+}
+
+Console.WriteLine("My first guess: audio");
+
+while(true){
+    Console.Write("Input letters that are not in the word: (e.g. asdf)\n");
+    string input = Console.ReadLine();
+    foreach(char character in input.ToLower())
+    {
+        badCharacters.Add(getNumber(character));
+    }
+    Console.WriteLine("My guess: "+getGuess());
+}
