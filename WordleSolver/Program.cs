22e2275f9c251c7478db2f99b0b384b2ece3a137
leigh966 <leigh966@hotmail.com>
2023-12-21 16:44:37 +0000

    now inferring good letters and assuring that they are in the next guess. This would likely work with a better dictionary

--- WordleSolver/Program.cs
+++ WordleSolver/Program.cs
@@ -1,13 +1,30 @@
 ﻿using LeggySetLib;
 using System;
+using System.Net;
 
 string[] allWords = File.ReadAllLines("words.txt");
 
-ISet<int> badCharacters = new BinarySet64(0,25);
+ISet<int> badCharacters = new BinarySet32(0,25), goodCharacters = new BinarySet32(0,25);
 
+
+char getLetter(int number)
+{
+    return (char)(number + 97);
+}
+
+
+bool goodCharactersCovered(string word)
+{
+    foreach (int number in goodCharacters)
+    {
+        if (!word.Contains(getLetter(number))) return false;
+    }
+    return true;
+}
 
 bool couldBeThisWord(string word)
 {
+    if(!goodCharactersCovered(word)) return false;
     foreach (char letter in word)
     {
         if (badCharacters.Contains(getNumber(letter)))
@@ -36,13 +53,27 @@ int getNumber(char letter)
 }
 
 Console.WriteLine("My first guess: audio");
-
-while(true){
+string lastGuess = "audio";
+void getInput()
+{
     Console.Write("Input letters that are not in the word: (e.g. asdf)\n");
     string input = Console.ReadLine();
-    foreach(char character in input.ToLower())
+    foreach (char character in input.ToLower())
     {
         badCharacters.Add(getNumber(character));
     }
-    Console.WriteLine("My guess: "+getGuess());
+    foreach(char character in lastGuess)
+    {
+        int charNumber = getNumber(character);
+        if (!badCharacters.Contains(charNumber))
+        {
+            goodCharacters.Add(charNumber);
+        }
+    }
+}
+
+while(true){
+    getInput();
+    lastGuess = getGuess();
+    Console.WriteLine("My guess: "+lastGuess);
 }
